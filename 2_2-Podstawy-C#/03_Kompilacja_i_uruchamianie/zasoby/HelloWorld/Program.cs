9e42f0e06615d6779f701d9c43ddf96c18da59ac
kasperdob <kasperdob@gmail.com>
2025-10-02 20:59:47 +0200

    Dodanie lekcji 2_2_01 2_2_02 2_2_03

---
+++ 2_2-Podstawy-C#/03_Kompilacja_i_uruchamianie/zasoby/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj, świecie!");
+            Console.ReadLine();
+        }
+    }
+}
