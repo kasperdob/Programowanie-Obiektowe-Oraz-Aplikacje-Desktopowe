2985ef52ccdba96777d9adc3e37d59d2c6db6eb5
kasperdob <kasperdob@gmail.com>
2025-10-03 15:39:55 +0200

    Dodanie lekcji 46 i 48

---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Konstruktory/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Konstruktory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uczen jan = new Uczen("Jan", 16);
+            jan.PrzedstawSie();  // Wynik: Cześć, mam na imię Jan i mam 16 lat.
+        }
+    }
+}
---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Konstruktory/Uczen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory
+{
+    class Uczen
+    {
+        public string Imie;
+        public int Wiek;
+
+        // Konstruktor
+        public Uczen(string imie, int wiek)
+        {
+            Imie = imie;
+            Wiek = wiek;
+        }
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine($"Cześć, mam na imię {Imie} i mam {Wiek} lat.");
+        }
+    }
+}
