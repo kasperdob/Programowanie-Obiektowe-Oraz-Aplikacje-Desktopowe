e76136f18ed8de2a0f8b7e5961a7cd2318d03ade
kasperdob <kasperdob@gmail.com>
2025-10-03 16:57:27 +0200

    Dodanie lekcji kod do lekcji 46

---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Konstruktory2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Konstruktory2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uczen uczen = new Uczen("Jan", "Kowalski", 16);
+
+            var listaObecnoisci = new List<int> { 1, 1, 0, 1, 1 };
+            var listaOcen = new List<int> { 5, 4, 3, 4, 5 };
+
+            Uczen uczen1 = new Uczen("Piotr", "Kowalski", 16, listaObecnoisci, listaOcen);
+
+            Console.WriteLine("oceny ucznia Jan");
+            foreach (var ocena in uczen.Oceny)
+            {
+                Console.WriteLine(ocena);
+            }
+
+            Console.WriteLine("oceny ucznia Piotr");
+            foreach (var ocena in uczen1.Oceny)
+            {
+                Console.WriteLine(ocena);
+            }
+        }
+    }
+
+}
---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Konstruktory2/Uczen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory2
+{
+    public class Uczen
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public List<int> LiczbaObecnosci { get; set; }
+        public List<int> Oceny { get; set; }
+
+        public Uczen(string Imie, string Nazwisko, int Wiek) 
+        {
+            this.Imie = Imie;
+            this.Nazwisko = Nazwisko;
+            this.Wiek = Wiek;
+
+            LiczbaObecnosci = new List<int>();
+            Oceny = new List<int>();
+        }
+
+        public Uczen(
+            string Imie, 
+            string Nazwisko, 
+            int Wiek,
+            List<int> LiczbaObecnosci,
+            List<int> Oceny)
+        {
+            this.Imie = Imie;
+            this.Nazwisko = Nazwisko;
+            this.Wiek = Wiek;
+            this.LiczbaObecnosci = LiczbaObecnosci;
+            this.Oceny = Oceny;
+        }
+
+        public bool CzyKlasyfikowany() 
+        {
+            var liczbaOcen = Oceny.Count;
+            var liczbaObecnosci = LiczbaObecnosci.Count();
+
+            if (liczbaOcen >= 3 && liczbaObecnosci > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double SredniaOcen()
+        {
+            if (Oceny.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Oceny.Average();
+            }
+        }
+
+        
+    }
+}
