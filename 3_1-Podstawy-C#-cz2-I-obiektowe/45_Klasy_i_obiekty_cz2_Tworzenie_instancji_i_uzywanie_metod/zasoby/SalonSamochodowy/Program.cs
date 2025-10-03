a0ae74030c31165ef20c6f721b003c0b3625c131
kasperdob <kasperdob@gmail.com>
2025-10-03 14:56:07 +0200

    Dodanie lekcji 45

---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/45_Klasy_i_obiekty_cz2_Tworzenie_instancji_i_uzywanie_metod/zasoby/SalonSamochodowy/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SalonSamochodowy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochod mercedes = new Samochod();
+
+            mercedes.Marka = "Mercedes";
+            mercedes.Kolor = "Czarny";
+            mercedes.RokProdukcji = 2020;
+            mercedes.Dostepnosc = true;
+
+            Samochod audi = new Samochod();
+
+            audi.Marka = "Audi";
+            audi.Kolor = "Bialy";
+            audi.RokProdukcji = 2019;
+            audi.Dostepnosc = false;
+
+
+            mercedes.UruchomSilnik();
+            mercedes.Go();
+
+            audi.Go();
+        }
+    }
+}
---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/45_Klasy_i_obiekty_cz2_Tworzenie_instancji_i_uzywanie_metod/zasoby/SalonSamochodowy/Samochod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonSamochodowy
+{
+    public class Samochod
+    {
+        public string Marka { get; set; }
+        public string Kolor { get; set; }
+        public int RokProdukcji { get; set; }
+        public bool Dostepnosc { get; set; }
+
+        private bool silnikUruchomiony = false;
+
+        public void UruchomSilnik()
+        {
+            silnikUruchomiony = true;
+            Console.WriteLine($"Uruchiony silnik w {Marka}");
+        }
+
+        public void Go()
+        {
+            if (silnikUruchomiony)
+            {
+                Console.WriteLine($"{Marka} ruszyło");
+            }
+            else
+            {
+                Console.WriteLine($"Najpierw uruchom silnik");
+            }
+        }
+
+        public string ZwrocOferteSprzedaz()
+        {
+            if (Dostepnosc)
+            {
+                return $"{Marka} {Kolor} {RokProdukcji}";
+            }
+            else 
+            {
+                return $"Brak oferty sprzedaży";
+            }
+        }
+    }
+}
