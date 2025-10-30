df4d4723e65bfa6e49073208cd5510604020aa86
kasperdob <kasperdob@gmail.com>
2025-10-30 13:55:54 +0100

    dodanie kody do sklepu

---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Sklep/Kategoria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep
+{
+    public class Kategoria
+    {
+        public string Nazwa { get; set; }
+        public List<Produkt> Produkty { get; set; } = new List<Produkt>();
+        public Kategoria(string nazwa)
+        {
+            Nazwa = nazwa;
+        }
+
+        public Produkt DodajProdukt(string nazwa, decimal cena)
+        {
+            Produkt produkt = new Produkt(nazwa, cena);
+            Produkty.Add(produkt);
+            return produkt;
+        }
+
+        public Produkt ZnajdzProdukt(string nazwa)
+        {
+            return Produkty.FirstOrDefault(p => p.Nazwa == nazwa);
+        }
+
+        public void UsunProdukt(string nazwa)
+        {
+            Produkt produkt = ZnajdzProdukt(nazwa);
+            if (produkt != null)
+            {
+                Produkty.Remove(produkt);
+            }
+        }
+
+        public void WyswietlProdukty()
+        {
+            Console.WriteLine($"Kategoria: {Nazwa}");
+            foreach (var produkt in Produkty)
+            {
+                Console.WriteLine($"- {produkt.Nazwa}: {produkt.Cena} PLN");
+            }
+        }
+    }
+}
---
+++ 3_1-Podstawy-C#-cz2-I-obiektowe/46_Konstruktory/zasoby/Sklep/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sklep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kategoria elektronika = new Kategoria("Elektronika");
+
+            elektronika.DodajProdukt("Laptop", 2000);
+            elektronika.DodajProdukt("Smartfon", 1500);
+            elektronika.DodajProdukt("Tablet", 800);
+
+            elektronika.WyswietlProdukty();
+        }
+    }
+}
